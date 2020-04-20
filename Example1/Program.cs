f649c2beadd5f8be4bdefb9b370b7bcb63970077
Marc Marañón Vidal <mmaravidal@outlook.es>
2020-04-20 17:59:20 +0200

    Initial commit (#1)
    * Initial commit
    
    * Added missing Done calls
    
    * Project renamed
    
    Co-authored-by: Marc Marañon <marc.maranon@scrm.eu>

---
+++ Example1/Program.cs
@@ -0,0 +1,87 @@
+﻿using ColoredConsole;
+using System;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WhenSuccess();
+
+            WhenFirstFails();
+
+            WhenLastFails();
+        }
+
+        static async Task WhenSuccess()
+        {
+            ColorConsole.WriteLine();
+            ColorConsole.WriteLine("Simulating a successful transaction".DarkGreen());
+
+            using (var transaction = new TransactionScope())
+            {
+                var repository = new TransactionalService("Repository", false);
+                var publisher = new TransactionalService("Publisher", false);
+
+                var item = Guid.NewGuid().ToString();
+                repository.Add(item);
+                publisher.Add(item);
+
+                transaction.Complete();
+            }
+        }
+
+        static async Task WhenFirstFails()
+        {
+            ColorConsole.WriteLine();
+            ColorConsole.WriteLine("Simulating a first agent failing transaction".DarkGreen());
+
+            try
+            {
+                using (var transaction = new TransactionScope())
+                {
+                    var repository = new TransactionalService("Repository", true);
+                    var publisher = new TransactionalService("Publisher", false);
+
+                    var item = Guid.NewGuid().ToString();
+                    repository.Add(item);
+                    publisher.Add(item);
+
+                    transaction.Complete();
+                }
+            }
+            catch (Exception exception)
+            {
+                ColorConsole.WriteLine(exception.Message.DarkRed());
+            }
+        }
+
+        static async Task WhenLastFails()
+        {
+            ColorConsole.WriteLine();
+            ColorConsole.WriteLine("Simulating a last agent failing transaction".DarkGreen());
+
+            try
+            {
+                using (var transaction = new TransactionScope())
+                {
+                    var repository = new TransactionalService("Repository", false);
+                    var publisher = new TransactionalService("Publisher", true);
+
+                    var item = Guid.NewGuid().ToString();
+                    repository.Add(item);
+                    publisher.Add(item);
+
+                    transaction.Complete();
+                }
+            }
+            catch (Exception exception)
+            {
+                ColorConsole.WriteLine(exception.Message.DarkRed());
+            }
+        }
+    }
+}
---
+++ Example1/TransactionalService.cs
@@ -0,0 +1,67 @@
+﻿using ColoredConsole;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Transactions;
+
+namespace Cli
+{
+    public class TransactionalService : IEnlistmentNotification
+    {
+        private readonly List<string> _items = new List<string>();
+        private readonly string _name;
+        private readonly bool _throws;
+
+        public TransactionalService(string name, bool throws)
+        {
+            _name = name;
+            _throws = throws;
+
+            // Enlist the service within the transaction
+            Transaction.Current.EnlistVolatile(this, EnlistmentOptions.None);
+        }
+
+        public void Commit(Enlistment enlistment)
+        {
+            ColorConsole.WriteLine(_name, ": ", "Executing Commit()".Black().OnDarkGray());
+
+            if (_throws) throw new InvalidOperationException(_name + " throws");
+
+            foreach (var item in _items)
+                ColorConsole.WriteLine(_name, ": ", item.DarkGray());
+
+            enlistment.Done();
+        }
+
+        public void Add(string item)
+        {
+            _items.Add(item);
+        }
+
+        public void InDoubt(Enlistment enlistment)
+        {
+            ColorConsole.WriteLine(_name, ": ", "Executing InDount()".Black().OnDarkGray());
+
+            // Do nothing. This method is used when the transaction is in doubt, i.e: a failure occurred in any agent while commiting
+            enlistment.Done();
+        }
+
+        public void Prepare(PreparingEnlistment preparingEnlistment)
+        {
+            ColorConsole.WriteLine(_name, ": ", "Executing Prepare()".Black().OnDarkGray());
+
+            // Do nothing. This method is used to prepare the data for the commit phase.
+            preparingEnlistment.Prepared();
+        }
+
+        public void Rollback(Enlistment enlistment)
+        {
+            ColorConsole.WriteLine(_name, ": ", "Executing Rollback()".Black().OnDarkGray());
+
+            // Rollback the transaction
+            _items.Clear();
+
+            enlistment.Done();
+        }
+    }
+}
