0054e184959264b7bf56e27fc313130846bc6175
Marc Marañón Vidal <mmaravidal@outlook.es>
2020-04-21 08:50:16 +0200

    Added Example 2 (#2)
    Co-authored-by: Marc Marañon <marc.maranon@scrm.eu>

---
+++ Example2/Models/CreateOrderRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Example2
+{
+    public class CreateOrderRequest
+    {
+        public string Name { get; set; }
+        public Dictionary<string, int> Items { get; set; }
+    }
+}
